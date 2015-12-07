a150ee84b7b4059cc44c7cddae1b25f6236d7429
Michael Gardner <gardnm4@rpi.edu>
2015-12-06 23:59:22 -0500

    Day1 Prob2 in haskell
    Fixed a small bug in solution 1 where it ran twice and had an error the
    second time. Now it only runs once, gives an answer, and stops. I also
    have an answer for problem 2 in Haskell. Same pattern for running it.

--- day1/prob0.hs
+++ day1/prob0.hs
@@ -5,7 +5,7 @@ import Data.Char
 countLetters :: String -> Char -> Int
 countLetters str c = length $ filter (== c) str
 
-main = forever $ do
+main = do
     putStr "Give me some input: "
     l <- getLine
     print ((countLetters l '(')-(countLetters l ')'))
---
+++ day1/prob1.hs
@@ -0,0 +1,18 @@
+import Control.Monad
+import Data.Char
+
+
+
+negfloor :: String -> Int -> Int -> Int
+negfloor "" c f = f
+negfloor str@(x:xs) c f
+  | c == -1 = f
+  | x == '(' = (negfloor xs (c+1) (f+1))
+  | x == ')' = (negfloor xs (c-1) (f+1))
+
+
+
+main =  do
+    putStr "Give me some input: "
+    l <- getLine
+    print (negfloor l 0 0)
