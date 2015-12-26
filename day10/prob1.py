9836edcd27b92e19ab51917ef3a3b31070aae796
Michael Gardner <gardnm4@rpi.edu>
2015-12-25 23:30:39 -0500

    Solution for part 2.
    Same things but changed 40 to 50.

---
+++ day10/prob1.py
@@ -0,0 +1,24 @@
+word = "3113322113"
+result = ""
+count =0;
+for i in range(50):
+
+    if len(word) == 1:
+        result += "1" + word
+    else:
+        for j in range(len(word)-1):
+            curr = word[j]
+            count+=1
+            if (curr != word[j+1]):
+                result += str(count)+curr
+                count=0
+        if (curr == word[len(word)-1]):
+            result += str(count+1)+curr
+        else:
+            result += "1" + word[len(word)-1]
+
+    word = result
+    result = ""
+
+
+print len(word)
