a1b67a82b9a7031f88fff24102e3947c919c7140
MJGTwo <gardner.j.michael@gmail.com>
2023-06-29 22:19:58 -0400

    rust day1p2

--- 15/rust/src/d1.rs
+++ 15/rust/src/d1.rs
@@ -11,6 +11,25 @@ fn find_floor( seq: &str) -> usize {
      seq.matches('(').count() - seq.matches(')').count()
 }
 
+fn move_elevator(mv: char) -> i32 {
+    match mv {
+        '(' => 1,
+        ')' => -1,
+        _ => 0
+    }
+}
+
+fn find_basement(seq: &str) -> usize {
+    let mut level = 0;
+    for (index, letter) in seq.chars().enumerate() {
+        level += move_elevator(letter);
+        if level == -1 {
+            return index + 1;
+        }
+    }
+    return 0;
+}
+
 fn ex(){
     let test0 = "))(((((";
     let answer0 = 3;
@@ -27,5 +46,6 @@ fn p1(){
 }
 
 fn p2(){
-
+    let input = read_file_to_string("./src/d1.txt");
+    println!("{}",find_basement(input.as_str()));
 }
